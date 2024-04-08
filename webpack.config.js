92dddb766c491771bb2cafa2447c0d8e8fb17d2e
Erick Muriithi <muriithierick3758@gmail.com>
2024-04-08 11:37:05 +0300

    added HMR in the config.js file

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,21 @@
 const path = require('path');
-
+const webpack  = require('webpack');
 module.exports = {
   // Entry point for your application
   entry: './src/index.js',
+  devServer:{
+    hot:true,
+    port:9000,
+  },
+  plugins:[
+    new webpack.HotModuleReplacementPlugin(),
+
+  ],
 
   // Output configuration
   output: {
     filename: 'bundle.js', // Name of the generated bundle
     path: path.resolve(__dirname, 'dist'), // Output directory for the bundle
   },
+  mode:"development"
 };
