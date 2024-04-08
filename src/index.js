1fff2b3afd0e99c215e023552ba82678c108bca7
Erick Muriithi <muriithierick3758@gmail.com>
2024-04-08 12:35:06 +0300

    default user profile,implement the bundling in the file

---
+++ src/index.js
@@ -0,0 +1,10 @@
+// Import a logging module
+
+import './style.css';
+import './style.scss';
+import  display from './bid.png'
+
+
+
+
+console.log('The phrase  "Hello world" contains the following number of counts',);
