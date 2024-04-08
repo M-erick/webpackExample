3d3e14d10de7f53f5308b5cdbb5ed210c3cd492b
Erick Muriithi <muriithierick3758@gmail.com>
2024-04-08 15:14:25 +0300

    return an array of objects

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 // index.js
-import { getUsername, getPassword, authenticateUser } from './userData.js';
-import displayImage from './ticketing.jpeg';
+import { getUsernames, getPassword, authenticateUser } from './userData.js';
 
+const  usernames = [{
+    name:"Erick",Age:10,gmail:"emcytonn@gmail.com"
+},
+{name:"james",Age:14,gmail:"jmcytonn@gmail.com"}];
 // Use the imported functions
-console.log("Username:",getUsername()); 
+console.log("Username:",getUsernames(usernames)); 
 console.log("Password:",getPassword()); 
 console.log("Note:",authenticateUser()); 
