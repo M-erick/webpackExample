f0b8b1c338d0643549d3e5097d4f6d9ef01519b8
Erick Muriithi <muriithierick3758@gmail.com>
2024-04-08 14:44:20 +0300

    dummy user records

---
+++ src/userData.js
@@ -0,0 +1,13 @@
+export function getUsername(){
+    
+    return "Erick Muriithi";
+}
+
+export function getPassword() {
+    return "james1234";
+}
+export function authenticateUser(){
+    return "Please  username and password";
+}
+
+// export{getUsername,getPassword,authenticateUser};
